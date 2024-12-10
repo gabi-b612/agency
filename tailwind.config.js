fcdb52e90025a4337e6cabfe7973af21c75a5753
gabi-b612 <20km095@esisalama.org>
2024-12-10 12:23:22 +0200

    fixed navBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'neutralSiver':'#F5F7FA',
+        'neutralDGrey':'#4D4D4D',
+        'brandPrimary':'#4CAF4F',
+        'neutralSiver':'#717171',
+        'gray900':'#18191F',
+      }
+    },
   },
   plugins: [],
 }
