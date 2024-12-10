88c1ffb942427d814623c271a6d5341741b6a1d2
gabi-b612 <20km095@esisalama.org>
2024-12-10 14:32:08 +0200

    Fixed hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -10,11 +13,11 @@ export default {
         'neutralSiver':'#F5F7FA',
         'neutralDGrey':'#4D4D4D',
         'brandPrimary':'#4CAF4F',
-        'neutralSiver':'#717171',
+        // 'neutralSilver':'#717171',
         'gray900':'#18191F',
       }
     },
   },
-  plugins: [],
+  plugins: [ flowbite.plugin()],
 }
 
